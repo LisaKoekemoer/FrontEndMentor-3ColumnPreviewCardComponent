8c35b6a71e5d66d98824d1586d0fe89d872ccf28
Alicia <alicia.koekemoer98@gmail.com>
2024-06-09 15:07:03 +0200

    Complete 3-Column Preview Card Component Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import luxury from './images/icon-luxury.svg';
+import sedan from './images/icon-sedans.svg';
+import suv from './images/icon-suvs.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='card-component'>
+
+        <div className='sedans'>
+          <img src={sedan} alt=""/>
+          <h1>SEDANS</h1>
+          <p>Choose a sedan for its affordability and excellent 
+            fuel economy. Ideal for cruising in the city or on your 
+            next road trip.</p>
+          <button type="submit">Learn More</button>
+        </div>
+
+        <div className='suvs'>
+          <img src={suv} alt=""/>
+          <h1>SUVS</h1>
+          <p>Take an SUV for its spacious interior, 
+            power, and versatility. Perfect for your 
+            next family vacation and off-road adventures.</p>
+          <button type="submit">Learn More</button>
+        </div>
+          
+        <div className='luxury'>
+          <img src={luxury} alt=""/>
+          <h1>LUXURY</h1>
+          <p>Cruise in the best car brands without the 
+             bloated prices. Enjoy the enhanced comfort 
+             of a luxury rental and arrive in style.</p>
+          <button type="submit">Learn More</button>
+        </div>
+        
+      </div>
+    </>
   );
 }
 
